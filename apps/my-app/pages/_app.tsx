f8c72fde7c8b596e6518cec88327c4287b549b6b
Juliano Brasil <juliano.brasil@gmail.com>
2025-12-01 11:28:44 -0300

    remove authentication

--- apps/my-app/pages/_app.tsx
+++ apps/my-app/pages/_app.tsx
@@ -1,18 +1,17 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <>
       <Head>
         <title>Welcome to !</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </SessionProvider>
+    </>
   );
 }
 
--- apps/my-app/proxy.ts
+++ apps/my-app/proxy.ts
@@ -1,26 +1,11 @@
-import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 import type { NextRequestWithAuth } from 'next-auth/middleware';
 
-export default withAuth(
-  async function proxy(request: NextRequestWithAuth) {
-    console.log('Hit: ', request.nextUrl.pathname);
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: async ({ req: { nextUrl: { pathname } }, token }) => {
-        console.log('Pathname: ', pathname);
-        // if (['/api/auth', 'auth'].some(path => pathname.startsWith(path))) {
-        //   return true;
-        // }
 
-        // Return true if user is authenticated, false otherwise
-        return !!token;
-      },
-    },
-  }
-);
+export default async function proxy(request: NextRequestWithAuth) {
+  console.log('Hit: ', request.nextUrl.pathname);
+  return NextResponse.next();
+}
 
 // export const config = {
 //   matcher: [
